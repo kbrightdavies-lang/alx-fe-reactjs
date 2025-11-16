d3b3a3bbdf1187a928fd3137140942c907ae61c8
Kelly-Ann Bright-Davies <kbrightdavies@gmail.com>
2025-11-16 21:32:10 +0000

    Included router code in app.jsx and created EditRecipeForm.jsx, DeleteRecipeForm.jsx and updated Zustand store.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,29 @@
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from '.src/components/RecipeList';
+import AddRecipeForm from '.src/components/AddRecipeForm';
+import RecipeDetails from '.src/components/RecipeDetails';
+
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <AddRecipeForm />
+            <RecipeList />
+          </>
+        } />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
+    </Router>
+  );
 }
+
+// Wrapper to extract the :id param and pass it as recipeId
+import { useParams } from 'react-router-dom';
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={Number(id)} />;
+};
 
 export default App;
