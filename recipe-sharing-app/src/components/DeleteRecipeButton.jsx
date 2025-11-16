2ddf3bfb6b539db27477bfdc2d5816528ddc6d87
Kelly-Ann Bright-Davies <kbrightdavies@gmail.com>
2025-11-16 21:41:34 +0000

    Added useNavigate to DeleteRecipeButton.jsx

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -2,8 +2,11 @@ import { useRecipeStore } from './recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+  const navigate = useNavigate(); // <-- this is the hook
+
 
   return <button onClick={() => deleteRecipe(recipeId)}>Delete Recipe</button>;
+  navigate("/");            // send user back to home
 };
 
 export default DeleteRecipeButton;
