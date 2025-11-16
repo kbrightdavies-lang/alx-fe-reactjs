39733687660d7fb1dea665af4a83a2db87845e8f
Kelly-Ann Bright-Davies <kbrightdavies@gmail.com>
2025-11-16 20:12:57 +0000

    Recreated recipe-sharing-app, components, Zustand store, RecipeList and AddRecipeForm, deleted assets.

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,25 @@
+// RecipeList component
+  import { Link } from "react-router-dom";
+  import { useRecipeStore } from '.store/recipeStore';
+
+  const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
+
+    return (
+      <div>
+        <h2>Recipes</h2>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+
+            <link to={`/recipe/${recipe.id}`}>
+            <button>View Details</button>
+            </link>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  export default RecipeList;
