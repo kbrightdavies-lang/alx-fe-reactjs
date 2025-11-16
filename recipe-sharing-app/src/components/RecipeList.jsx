d5d082698ba38b4845ed623001ee51424ea0bbcd
Kelly-Ann Bright-Davies <kbrightdavies@gmail.com>
2025-11-16 20:58:04 +0000

    Moved recipeStore from store to components

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,6 @@
 // RecipeList component
   import { Link } from "react-router-dom";
-  import { useRecipeStore } from '.store/recipeStore';
+  import { useRecipeStore } from '.src/components/recipeStore';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
